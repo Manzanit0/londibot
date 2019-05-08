fc8d94338c89172a5ffb64d5f7bd2602263708fa
Manzanit0 <unico.javier@gmail.com>
2019-05-08 13:47:23 +0100

    Adds Notification structure and adapts notifier.

--- lib/core/notifier.ex
+++ lib/core/notifier.ex
@@ -1,22 +1,25 @@
+defmodule Londibot.Notification do
+  defstruct [:message, :channel_id]
+end
+
 defmodule Londibot.NotifierBehaviour do
-  @callback send(String.t, String.t) :: String.t
+  @callback send(%Londibot.Notification{}) :: String.t()
 end
 
 defmodule Londibot.Notifier do
+  alias Londibot.Notification
+
   @behaviour Londibot.NotifierBehaviour
 
   @slack_url "https://slack.com/api/chat.postMessage"
   @slack_token Application.get_env(:londibot, :slack_token)
 
- def send(message, channel_id) do
-   encoded_message = URI.encode(message)
-   encoded_id = URI.encode(channel_id)
+  def send(%Notification{channel_id: channel_id, message: message}) do
+    encoded_message = URI.encode(message)
+    encoded_id = URI.encode(channel_id)
 
-   "#{@slack_url}?token=#{@slack_token}&channel=#{encoded_id}&text=#{encoded_message}"
-   |> HTTPoison.post!("") # Empty body.
- end
-end
-
-defmodule Londibot.Notification do
-  defstruct [:message, :channel_id]
+    "#{@slack_url}?token=#{@slack_token}&channel=#{encoded_id}&text=#{encoded_message}"
+    # Empty body.
+    |> HTTPoison.post!("")
+  end
 end
