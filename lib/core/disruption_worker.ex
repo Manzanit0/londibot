dcc399c46e2e065d0d0dd47e72f13196273e5367
Manzanit0 <unico.javier@gmail.com>
2019-05-08 13:49:56 +0100

    Adds function to DisruptionWorker to prompt TFL service for disruptions and wrap them into notifications.

--- lib/core/disruption_worker.ex
+++ lib/core/disruption_worker.ex
@@ -3,13 +3,21 @@ defmodule Londibot.DisruptionWorker do
   alias Londibot.Notification
 
   @subscription_store Application.get_env(:londibot, :subscription_store)
+  @tfl_service Application.get_env(:londibot, :tfl_service)
+
+  def disruption_notifications do
+    @tfl_service.lines
+    |> @tfl_service.status
+    |> @tfl_service.disruptions
+    |> create_notifications
+  end
 
   def create_notifications(disruptions) do
     subscriptions = @subscription_store.all()
 
     for {line, status, _} <- disruptions,
-      %Subscription{channel_id: channel, tfl_lines: lines} <- subscriptions,
-      Enum.any?(lines, fn x -> x == line end),
-      do: %Notification{message: ~s(#{line}: #{status}), channel_id: channel}
+        %Subscription{channel_id: channel, tfl_lines: lines} <- subscriptions,
+        Enum.any?(lines, fn x -> x == line end),
+        do: %Notification{message: ~s(#{line}: #{status}), channel_id: channel}
   end
 end
--- test/core/disruption_worker_test.exs
+++ test/core/disruption_worker_test.exs
@@ -9,35 +9,69 @@ defmodule Londibot.DisruptionWorkerTest do
 
   test "generates notifications for two subscriptions to the same disruption" do
     Application.get_env(:londibot, :subscription_store)
-    |> expect(:all,
-      fn -> [
-        %Subscription{id: 1, channel_id: "123QWE", tfl_lines: ["Victoria"]},
-        %Subscription{id: 1, channel_id: "456RTY", tfl_lines: ["Victoria"]}]
-      end)
+    |> expect(
+      :all,
+      fn ->
+        [
+          %Subscription{id: 1, channel_id: "123QWE", tfl_lines: ["Victoria"]},
+          %Subscription{id: 1, channel_id: "456RTY", tfl_lines: ["Victoria"]}
+        ]
+      end
+    )
 
     notifications =
       [{"Victoria", "Bad Service", "..."}]
-      |> DisruptionWorker.create_notifications
+      |> DisruptionWorker.create_notifications()
 
     assert notifications == [
-      %Notification{message: "Victoria: Bad Service", channel_id: "123QWE"},
-      %Notification{message: "Victoria: Bad Service", channel_id: "456RTY"}]
+             %Notification{message: "Victoria: Bad Service", channel_id: "123QWE"},
+             %Notification{message: "Victoria: Bad Service", channel_id: "456RTY"}
+           ]
   end
 
   test "generates notifications for two subscriptions to different disruptions" do
     Application.get_env(:londibot, :subscription_store)
-    |> expect(:all,
-      fn -> [
-        %Subscription{id: 1, channel_id: "123QWE", tfl_lines: ["Circle"]},
-        %Subscription{id: 2, channel_id: "456RTY", tfl_lines: ["Victoria"]}]
-      end)
+    |> expect(
+      :all,
+      fn ->
+        [
+          %Subscription{id: 1, channel_id: "123QWE", tfl_lines: ["Circle"]},
+          %Subscription{id: 2, channel_id: "456RTY", tfl_lines: ["Victoria"]}
+        ]
+      end
+    )
 
     notifications =
       [{"Victoria", "Minor delays", "..."}, {"Circle", "Minor delays", "..."}]
-      |> DisruptionWorker.create_notifications
+      |> DisruptionWorker.create_notifications()
 
     assert notifications == [
-      %Notification{message: "Victoria: Minor delays", channel_id: "456RTY"},
-      %Notification{message: "Circle: Minor delays", channel_id: "123QWE"}]
+             %Notification{message: "Victoria: Minor delays", channel_id: "456RTY"},
+             %Notification{message: "Circle: Minor delays", channel_id: "123QWE"}
+           ]
+  end
+
+  test "prompts TFL for disruptions and generates notifications" do
+    Application.get_env(:londibot, :subscription_store)
+    |> expect(
+      :all,
+      fn ->
+        [
+          %Subscription{id: 1, channel_id: "123QWE", tfl_lines: ["Circle"]},
+          %Subscription{id: 2, channel_id: "456RTY", tfl_lines: ["Victoria"]}
+        ]
+      end
+    )
+
+    Application.get_env(:londibot, :tfl_service)
+    |> expect(:lines, fn -> ["victoria", "circle"] end)
+    |> expect(:status, fn _ ->
+      [{"Victoria", "Good Service", nil}, {"Circle", "Minor Delays", "Due to blablabla"}]
+    end)
+    |> expect(:disruptions, fn _ -> [{"Circle", "Minor Delays", "Due to blablabla"}] end)
+
+    notifications = DisruptionWorker.disruption_notifications()
+
+    assert notifications == [%Notification{message: "Circle: Minor Delays", channel_id: "123QWE"}]
   end
 end
