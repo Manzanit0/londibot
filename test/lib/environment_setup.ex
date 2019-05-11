ff64b57204b8cdb0f6e8d9554a8ead50cea43052
Manzanit0 <unico.javier@gmail.com>
2019-05-11 16:56:56 +0100

    Adds create function to EnvironmentSetup module to create the mocks.

--- test/lib/environment_setup.ex
+++ test/lib/environment_setup.ex
@@ -1,4 +1,6 @@
 defmodule EnvironmentSetup do
+  import Mox
+
   alias Londibot.Subscription
 
   defstruct [:disruptions, :subscriptions]
@@ -10,7 +12,18 @@ defmodule EnvironmentSetup do
   def with_subscription(env_setup, id, channel_id, lines) do
     with_subscription(env_setup, %Subscription{id: id, channel_id: channel_id, tfl_lines: lines})
   end
-  def with_subscription(e = %EnvironmentSetup{subscriptions: subscriptions}, s = %Subscription{}) do
+
+  def with_subscription(%EnvironmentSetup{subscriptions: subscriptions}, s = %Subscription{}) do
     %EnvironmentSetup{subscriptions: [s | subscriptions]}
+  end
+
+  def create(%EnvironmentSetup{subscriptions: subscriptions}) do
+    # Since the mocks set here are for all the tests throughout the
+    # project, and the project doesn't use the 'verify_on_exit' mechanic,
+    # I've decided to set a high number so the functions can be called
+    # as much as needed.
+    Application.get_env(:londibot, :subscription_store)
+    |> expect(:all, 99, fn -> subscriptions end)
+    |> expect(:fetch, 99, fn id -> Enum.find(subscriptions, &(&1.id == id)) end)
   end
 end
--- test/lib/environment_setup_test.exs
+++ test/lib/environment_setup_test.exs
@@ -1,5 +1,5 @@
 defmodule EnvironmentSetupTest do
-  use ExUnit.Case, async: :true
+  use ExUnit.Case, async: true
 
   alias Londibot.Subscription
 
@@ -12,7 +12,9 @@ defmodule EnvironmentSetupTest do
       EnvironmentSetup.new()
       |> EnvironmentSetup.with_subscription("id", "channel_id", ["victoria"])
 
-    assert subscriptions == [%Subscription{id: "id", channel_id: "channel_id", tfl_lines: ["victoria"]}]
+    assert subscriptions == [
+             %Subscription{id: "id", channel_id: "channel_id", tfl_lines: ["victoria"]}
+           ]
   end
 
   test "adds a subscription as struct" do
@@ -23,5 +25,31 @@ defmodule EnvironmentSetupTest do
       |> EnvironmentSetup.with_subscription(s)
 
     assert subscriptions == [s]
+  end
+
+  test "creates the environment mocks based on the setup struct" do
+    EnvironmentSetup.new()
+    |> EnvironmentSetup.with_subscription("5", "ASD890", ["victoria"])
+    |> EnvironmentSetup.with_subscription("9", "123QWE", ["circle", "bakerloo"])
+    |> EnvironmentSetup.create()
+
+    store = Application.get_env(:londibot, :subscription_store)
+
+    assert store.all() == [
+             %Subscription{id: "9", channel_id: "123QWE", tfl_lines: ["circle", "bakerloo"]},
+             %Subscription{id: "5", channel_id: "ASD890", tfl_lines: ["victoria"]}
+           ]
+
+    assert store.fetch("5") == %Subscription{
+             id: "5",
+             channel_id: "ASD890",
+             tfl_lines: ["victoria"]
+           }
+
+    assert store.fetch("9") == %Subscription{
+             id: "9",
+             channel_id: "123QWE",
+             tfl_lines: ["circle", "bakerloo"]
+           }
   end
 end
