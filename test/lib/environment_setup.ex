f9ad0266b9e76799b6e2d2be904fffe701edbaed
Manzanit0 <unico.javier@gmail.com>
2019-05-11 16:31:45 +0100

    Creates EnvironmentSetup module with function for adding subscriptions.

--- test/lib/environment_setup.ex
+++ test/lib/environment_setup.ex
@@ -1,5 +1,16 @@
 defmodule EnvironmentSetup do
-  def foo do
-    raise "oops"
+  alias Londibot.Subscription
+
+  defstruct [:disruptions, :subscriptions]
+
+  def new do
+    %EnvironmentSetup{disruptions: [], subscriptions: []}
+  end
+
+  def with_subscription(env_setup, id, channel_id, lines) do
+    with_subscription(env_setup, %Subscription{id: id, channel_id: channel_id, tfl_lines: lines})
+  end
+  def with_subscription(e = %EnvironmentSetup{subscriptions: subscriptions}, s = %Subscription{}) do
+    %EnvironmentSetup{subscriptions: [s | subscriptions]}
   end
 end
