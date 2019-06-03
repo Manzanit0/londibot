69cb9233224a3fa68e09f7542ebf34c9305e63fb
Manzanit0 <unico.javier@gmail.com>
2019-06-03 12:39:53 +0100

    :bug: Add Id to subscription on save
    Due to the upsert mechanic going on when saving subscriptions, all
    subscriptions being saved without an Id where overriding constantly that
    with id = nil.

--- lib/core/subscription_store.ex
+++ lib/core/subscription_store.ex
@@ -15,24 +15,19 @@ defmodule Londibot.SubscriptionStore do
 
   @behaviour Londibot.StoreBehaviour
 
-
   def start_link(s = %Subscription{}), do: start_link([s])
-  def start_link(arg) do
-    Agent.start_link(fn -> arg end, name: __MODULE__)
+  def start_link([]), do: Agent.start_link(fn -> [] end, name: __MODULE__)
+  def start_link(subscriptions) when is_list(subscriptions) do
+    Agent.start_link(fn -> [] end, name: __MODULE__)
+    Enum.each(subscriptions, &save/1)
   end
 
-  def all do
-    Agent.get(__MODULE__, & &1)
-  end
+  def all, do: Agent.get(__MODULE__, & &1)
 
-  def fetch(id) when is_integer(id) do
-    all()
-    |> Enum.find(fn subscription -> subscription.id == id end)
-  end
+  def fetch(id) when is_integer(id), do: Enum.find(all(), fn subscription -> subscription.id == id end)
 
-  def save(s = %Subscription{}) do
-    Agent.update(__MODULE__, &(upsert(&1, s)))
-  end
+  def save(s = %Subscription{id: nil}), do: save(%Subscription{s | id: System.unique_integer([:monotonic, :positive])})
+  def save(s), do: Agent.update(__MODULE__, &(upsert(&1, s)))
 
   defp upsert([], s = %Subscription{}), do: [s]
   defp upsert([%{id: id}|t], s = %Subscription{id: id}), do: [s|t]
