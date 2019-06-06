c70e8071d8c619b19b8eada3d8a2d85d3ee8a43d
Manzanit0 <unico.javier@gmail.com>
2019-06-06 16:07:41 +0100

    Replace SubscriptionHandler by CommandRunner

--- lib/core/commands/command_runner.ex
+++ lib/core/commands/command_runner.ex
@@ -1,7 +1,6 @@
 defmodule Londibot.Commands.CommandRunner do
   alias Londibot.Commands.Command
   alias Londibot.Subscription
-  alias Londibot.Web.SubscriptionHandler
 
   @tfl_service Application.get_env(:londibot, :tfl_service)
   @subscription_store Application.get_env(:londibot, :subscription_store)
@@ -19,18 +18,35 @@ defmodule Londibot.Commands.CommandRunner do
     |> to_text(:disruptions)
   end
 
-  def execute(%Command{command: "subscriptions", channel_id: c}) do
-    SubscriptionHandler.get_subscriptions(c)
+  def execute(%Command{command: "subscriptions", channel_id: channel_id}) do
+    subscriptions =
+      @subscription_store.all()
+      |> Enum.filter(fn %Subscription{channel_id: c} -> c == channel_id end)
+      |> Enum.map(fn x -> Map.get(x, :tfl_lines) end)
+      |> List.flatten()
+      |> Enum.join(", ")
+
+    subscription_list_message(subscriptions)
   end
 
   def execute(%Command{command: "subscribe", params: p, channel_id: c}) do
     subscription = %Subscription{channel_id: c, tfl_lines: p}
     @subscription_store.save(subscription)
 
-    SubscriptionHandler.subscription_saved_message()
+    subscription_saved_message()
   end
 
   def execute(_), do: {:error, "The command you just tried doesn't exist!"}
+
+  defp subscription_saved_message, do: to_payload("Subscription saved!")
+
+  defp subscription_list_message(""), do: to_payload("You are currently not subscribed to any line")
+  defp subscription_list_message(subscriptions),do: to_payload("You are currently subscribed to: " <> subscriptions)
+
+  defp to_payload(message) do
+    %{text: message, response_type: "in_channel"}
+    |> Poison.encode!()
+  end
 
   defp to_text(statuses, mode) when is_list(statuses) do
     statuses
