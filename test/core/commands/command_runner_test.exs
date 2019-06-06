3598ad843f7221887650e31e2f8eff80529f99a5
Manzanit0 <unico.javier@gmail.com>
2019-06-06 16:45:22 +0100

    Add tests to cover CommandRunner

--- test/core/commands/command_runner_test.exs
+++ test/core/commands/command_runner_test.exs
@@ -25,4 +25,42 @@ defmodule Londibot.Commands.CommandRunnerTest do
 
     assert "CIRCLE: Minor delays due to..." == CommandRunner.execute(%Command{command: "disruptions"})
   end
+
+  test "obtains current subscriptions for a given channel_id" do
+    World.new
+    |> World.with_subscription(45, "channel_id", "Victoria")
+    |> World.with_subscription(46, "channel_id", "London Overground")
+    |> World.create()
+
+    expected = "{\"text\":\"You are currently subscribed to: London Overground, Victoria\",\"response_type\":\"in_channel\"}"
+    assert expected == CommandRunner.execute(%Command{command: "subscriptions", channel_id: "channel_id"})
+  end
+
+  test "formats a no-subscriptions message" do
+    World.new
+    |> World.with_subscription(45, "channel_id", "Victoria")
+    |> World.with_subscription(46, "channel_id", "London Overground")
+    |> World.create()
+
+    expected = "{\"text\":\"You are currently not subscribed to any line\",\"response_type\":\"in_channel\"}"
+    assert expected == CommandRunner.execute(%Command{command: "subscriptions", channel_id: "wrong-channel_id"})
+  end
+
+  test "adds subscription to world" do
+    World.new
+    |> World.create()
+
+    expected = "{\"text\":\"Subscription saved!\",\"response_type\":\"in_channel\"}"
+    assert expected == CommandRunner.execute(%Command{command: "subscribe", params: "victoria, northern", channel_id: "channel_id"})
+  end
+
+
+  test "formats a friendly message upon inexistent command" do
+    World.new
+    |> World.create()
+
+    # TODO - this isn't consistent with the rest of the return types.
+    expected = {:error, "The command you just tried doesn't exist!"}
+    assert expected == CommandRunner.execute(%Command{command: ":)"})
+  end
 end
