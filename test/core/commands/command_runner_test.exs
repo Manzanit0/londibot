fa828fb567c002bce8a302b43b1321f3793be24c
Manzanit0 <unico.javier@gmail.com>
2019-06-06 16:06:53 +0100

    Fix tests and some warnings :(

--- test/core/commands/command_runner_test.exs
+++ test/core/commands/command_runner_test.exs
@@ -2,13 +2,14 @@ defmodule Londibot.Commands.CommandRunnerTest do
   use ExUnit.Case, async: true
 
   alias Londibot.Commands.CommandRunner
+  alias Londibot.Commands.Command
 
   test "formats line statuses as a report" do
     World.new
     |> World.with_disruption("Circle", "Minor Delays", "...")
     |> World.create()
 
-    result = CommandRunner.execute(:status)
+    result = CommandRunner.execute(%Command{command: "status"})
 
     # World prints returns the information for all the lines.
     # The interesting thing to test here is that CommandRunner returns
@@ -22,6 +23,6 @@ defmodule Londibot.Commands.CommandRunnerTest do
     |> World.with_disruption("Circle", "Minor Delays", "CIRCLE: Minor delays due to...")
     |> World.create()
 
-    assert "CIRCLE: Minor delays due to..." == CommandRunner.execute(:disruptions)
+    assert "CIRCLE: Minor delays due to..." == CommandRunner.execute(%Command{command: "disruptions"})
   end
 end
