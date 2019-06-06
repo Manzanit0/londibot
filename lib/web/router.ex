431df2cba5487491fab20e320a93108f312c514e
Manzanit0 <unico.javier@gmail.com>
2019-06-06 11:25:54 +0100

    Abstract the JSON header addition into a Plug
    By abstracting the JSON header addition into its own plug, it's now
    applied to every endpoint registered in the router, instead of only the
    /subscriptions endpoint.

--- lib/web/router.ex
+++ lib/web/router.ex
@@ -1,9 +1,9 @@
 defmodule Londibot.Router do
   use Plug.Router
 
-  alias Londibot.Web.Util
   alias Londibot.Web.SubscriptionHandler
 
+  plug Londibot.Web.DefaultHeadersPlug
   plug Plug.Parsers, parsers: [:urlencoded, :multipart]
   plug(:match)
   plug(:dispatch)
@@ -11,15 +11,7 @@ defmodule Londibot.Router do
   get("/", do: send_resp(conn, 200, "Service up and running!!"))
   post("/summary", do: send_resp(conn, 200, Londibot.Controller.report_all(:summary)))
   post("/disruptions", do: send_resp(conn, 200, Londibot.Controller.report_all(:disruptions)))
-
-  post "/subscription" do
-    conn =
-      conn
-      |> Plug.Conn.fetch_query_params()
-      |> Util.with_json_headers()
-
-    send_resp(conn, 200, SubscriptionHandler.handle(conn))
-  end
+  post("/subscription", do: send_resp(conn, 200, SubscriptionHandler.handle(conn)))
 
   match(_, do: send_resp(conn, 404, "Nothing found here!"))
 end
