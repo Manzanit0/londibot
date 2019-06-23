0cbe7587391a087d2bf7c2d8a4e54fa6865931ec
Manzanit0 <unico.javier@gmail.com>
2019-06-23 15:41:14 +0100

    Segregate app configuration by environment

--- config/config.exs
+++ config/config.exs
@@ -1,17 +1,3 @@
 use Mix.Config
 
-config :londibot, :slack_token, System.get_env("LONDIBOT_SLACK")
-
-if Mix.env == :test do
-  config :logger, level: :error
-
-  config :londibot, :tfl_service, Londibot.TFLMock
-  config :londibot, :notifier, Londibot.NotifierMock
-  config :londibot, :subscription_store, Londibot.SubscriptionStoreMock
-else
-  config :logger, level: :info
-
-  config :londibot, :tfl_service, Londibot.TFL
-  config :londibot, :notifier, Londibot.Notifier
-  config :londibot, :subscription_store, Londibot.SubscriptionStore
-end
+import_config "#{Mix.env()}.exs"
