16571d274e2af39d5015d0146b7fc56160ee9d98
Manzanit0 <unico.javier@gmail.com>
2019-06-23 16:56:15 +0100

    Clean up notifiers

--- lib/core/notifications/slack_notifier.ex
+++ lib/core/notifications/slack_notifier.ex
@@ -2,15 +2,17 @@ defimpl Londibot.Notifier, for: Londibot.SlackNotification do
   require Logger
 
   alias Londibot.SlackNotification, as: Notification
+  alias HTTPoison.Response
+  alias HTTPoison.Error
 
-  @slack_url "https://slack.com/api/chat.postMessage"
-  @slack_token Application.get_env(:londibot, :slack_token)
+  @endpoint "https://slack.com/api/chat.postMessage"
+  @token Application.get_env(:londibot, :slack_token)
 
   def send(%Notification{channel_id: channel_id, message: message}) do
     encoded_message = URI.encode(message)
     encoded_id = URI.encode(channel_id)
 
-    url = "#{@slack_url}?token=#{@slack_token}&channel=#{encoded_id}&text=#{encoded_message}"
+    url = "#{@endpoint}?token=#{@token}&channel=#{encoded_id}&text=#{encoded_message}"
 
     # Empty body -> message is in the URL
     case HTTPoison.post(url, "") do
@@ -19,23 +21,20 @@ defimpl Londibot.Notifier, for: Londibot.SlackNotification do
     end
   end
 
-  # Slack API only returns an error (4xx, 5xx...) upon an actual API error. Otherwise,
-  # it returns the errors in the body with a 200. That's why two different error handlers
-  # are needed.
-  defp handle_error(%HTTPoison.Response{request: %{url: url}} = resp, err) when is_binary(err) do
-    Logger.warn("\"#{err}\" thrown for request: #{url}")
-    {:error, resp}
-  end
-
-  defp handle_error(%HTTPoison.Error{reason: reason} = err) do
-    Logger.warn("slack returned an error with reason #{inspect(reason)}")
-    {:error, err}
-  end
-
-  defp handle_response(%HTTPoison.Response{body: body} = resp) do
+  defp handle_response(%Response{body: body} = resp) do
     case Poison.decode!(body) do
       %{"ok" => false, "error" => err} -> handle_error(resp, err)
       _ -> {:ok, resp}
     end
+  end
+
+  defp handle_error(%Response{request: %{url: url}} = resp, err) when is_binary(err) do
+    Logger.warn("\"#{err}\" thrown for request: #{url}")
+    {:error, resp}
+  end
+
+  defp handle_error(%Error{reason: reason} = err) do
+    Logger.warn("slack returned an error with reason #{inspect(reason)}")
+    {:error, err}
   end
 end
--- lib/core/notifications/telegram_notifier.ex
+++ lib/core/notifications/telegram_notifier.ex
@@ -2,38 +2,36 @@ defimpl Londibot.Notifier, for: Londibot.TelegramNotification do
   require Logger
 
   alias Londibot.TelegramNotification, as: Notification
+  alias HTTPoison.Response
+  alias HTTPoison.Error
 
   @token Application.get_env(:londibot, :telegram_token)
   @endpoint "https://api.telegram.org/bot#{@token}/sendMessage"
 
-  def send(%Notification{channel_id: channel_id, message: message}) do
-    body =
-      %{}
-      |> Map.put(:chat_id, channel_id)
-      |> Map.put(:text, message)
-      |> Map.put(:parse_mode, "markdown")
-      |> Poison.encode!()
+  def send(%Notification{channel_id: id, message: message}) do
+    headers = ["Content-Type": "Application/json"]
+    body = Poison.encode!(%{chat_id: id, text: message, parse_mode: "markdown"})
 
-    case HTTPoison.post(@endpoint, body, ["Content-Type": "Application/json"]) do
+    case HTTPoison.post(@endpoint, body, headers) do
       {:error, error} -> handle_error(error)
       {:ok, response} -> handle_response(response)
     end
   end
 
-  defp handle_error(%HTTPoison.Response{request: %{url: url}} = resp, err) when is_binary(err) do
-    Logger.warn("\"#{err}\" thrown for request: #{url}")
-    {:error, resp}
-  end
-
-  defp handle_error(%HTTPoison.Error{reason: reason} = err) do
-    Logger.warn("telegram returned an error with reason #{inspect(reason)}")
-    {:error, err}
-  end
-
-  defp handle_response(%HTTPoison.Response{body: body} = resp) do
+  defp handle_response(%Response{body: body} = resp) do
     case Poison.decode!(body) do
       %{"ok" => false, "description" => err} -> handle_error(resp, err)
       _ -> {:ok, resp}
     end
+  end
+
+  defp handle_error(%Response{request: %{url: url}} = resp, err) when is_binary(err) do
+    Logger.warn("\"#{err}\" thrown for request: #{url}")
+    {:error, resp}
+  end
+
+  defp handle_error(%Error{reason: reason} = err) do
+    Logger.warn("telegram returned an error with reason #{inspect(reason)}")
+    {:error, err}
   end
 end
