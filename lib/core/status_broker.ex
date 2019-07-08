4aff6bafce2cc358d49a2a1697f138acbe51e236
Manzanit0 <unico.javier@gmail.com>
2019-07-08 12:10:43 +0100

    Create StatusBroker module
    The StatusBroker's responsability is dealing with status changes in the
    TFL API and caching the latest status. Furthermore, it can also diff the
    changes, thus allowing other agents to only work with changing statuses.

---
+++ lib/core/status_broker.ex
@@ -0,0 +1,44 @@
+defmodule Londibot.StatusChange do
+  defstruct [:line, :previous_status, :new_status, :description]
+end
+
+defmodule Londibot.StatusBroker do
+  use Agent
+
+  alias Londibot.StatusChange
+
+  @tfl_service Application.get_env(:londibot, :tfl_service)
+
+  def start_link do
+    Agent.start_link(fn -> [] end, name: __MODULE__)
+  end
+
+  def get_latest do
+    @tfl_service.lines()
+    |> @tfl_service.status()
+    |> cache()
+  end
+
+  def get_cached do
+    Agent.get(__MODULE__, & &1)
+  end
+
+  def get_changes do
+    for {line2, old_status, _} <- get_cached(),
+        {line1, new_status, desc} <- get_latest(),
+        line1 == line2 and old_status != new_status,
+        into: [] do
+      %StatusChange{
+        line: line1,
+        previous_status: old_status,
+        new_status: new_status,
+        description: desc
+      }
+    end
+  end
+
+  defp cache(statuses) do
+    Agent.update(__MODULE__, fn _ -> statuses end)
+    statuses
+  end
+end
