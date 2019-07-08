a7fe6469da2a551d3f5d5b733bd06cf6a2657099
Manzanit0 <unico.javier@gmail.com>
2019-07-08 13:42:45 +0100

    Create Notifications from StatusChange structs

--- lib/core/notifications/notifications.ex
+++ lib/core/notifications/notifications.ex
@@ -10,12 +10,20 @@ defmodule Londibot.NotificationFactory do
   alias Londibot.Subscription
   alias Londibot.SlackNotification
   alias Londibot.TelegramNotification
+  alias Londibot.StatusChange
 
-  def create(%Subscription{service: :slack} = s, message) do
+  def create(%Subscription{service: :slack} = s, message)
+    when is_binary(message) do
     %SlackNotification{message: message, channel_id: s.channel_id}
   end
 
-  def create(%Subscription{service: :telegram} = s, message) do
+  def create(%Subscription{service: :telegram} = s, message)
+    when is_binary(message) do
     %TelegramNotification{message: message, channel_id: s.channel_id}
+  end
+
+  def create(%Subscription{} = s, %StatusChange{} = change) do
+    message = "#{change.line} line status has changed from #{change.previous_status} to #{change.new_status} (#{change.description})"
+    create(s, message)
   end
 end
--- test/core/notifications/notification_factory_test.exs
+++ test/core/notifications/notification_factory_test.exs
@@ -5,6 +5,7 @@ defmodule Londibot.NotificationFactoryTest do
   alias Londibot.Subscription
   alias Londibot.TelegramNotification
   alias Londibot.SlackNotification
+  alias Londibot.StatusChange
 
   test "creates a telegram notification" do
     s = %Subscription{service: :telegram, channel_id: "123"}
@@ -18,5 +19,39 @@ defmodule Londibot.NotificationFactoryTest do
 
     assert %SlackNotification{message: "Hey!", channel_id: "123"} ==
              NotificationFactory.create(s, "Hey!")
+  end
+
+  test "creates a Slack notification from a StatusChange" do
+    s = %Subscription{service: :slack, channel_id: "123"}
+    c = %StatusChange{
+        line: "victoria",
+        previous_status: "Good Service",
+        new_status: "Severe Delays",
+        description: "Due to passenger not minding the gap"
+      }
+
+      notification = NotificationFactory.create(s, c)
+
+      assert %SlackNotification{
+        message: "victoria line status has changed from Good Service to Severe Delays (Due to passenger not minding the gap)",
+        channel_id: "123"
+      } == notification
+  end
+
+  test "creates a Telegram notification from a StatusChange" do
+    s = %Subscription{service: :telegram, channel_id: "123"}
+    c = %StatusChange{
+        line: "victoria",
+        previous_status: "Good Service",
+        new_status: "Severe Delays",
+        description: "Due to passenger not minding the gap"
+      }
+
+      notification = NotificationFactory.create(s, c)
+
+      assert %TelegramNotification{
+        message: "victoria line status has changed from Good Service to Severe Delays (Due to passenger not minding the gap)",
+        channel_id: "123"
+      } == notification
   end
 end
