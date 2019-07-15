b4531d13fba3645f9b3fc16e8d81463be8c324a6
Manzanit0 <unico.javier@gmail.com>
2019-07-15 14:30:51 +0100

    Add status emoji to status report

--- lib/core/commands/command_runner.ex
+++ lib/core/commands/command_runner.ex
@@ -64,7 +64,12 @@ defmodule Londibot.Commands.CommandRunner do
 
   defp to_status_message(statuses) do
     statuses
-    |> Enum.map(fn {name, status, _} -> ~s(*#{name}:* #{status}) end)
+    |> Enum.map(&status/1)
     |> Enum.join("\n")
   end
+
+  defp status({name, "Good Service", _}), do: "✅ #{name}: Good Service"
+  defp status({name, "Closed", _}), do: "🚫 #{name}: Closed"
+  defp status({name, "Not Running", _}), do: "🚫 #{name}: Not Running"
+  defp status({name, status, _}), do: "⚠️ #{name}: #{status}"
 end
