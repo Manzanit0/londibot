e5b7859fa5670ec21e2a66a351e4557401849ce6
Manzanit0 <unico.javier@gmail.com>
2019-07-27 18:59:20 +0100

    Merge Web.Util into DefaultHeadersPlug

--- lib/londibot/web/default_headers_plug.ex
+++ lib/londibot/web/default_headers_plug.ex
@@ -1,11 +1,22 @@
 defmodule Londibot.Web.DefaultHeadersPlug do
-  alias Londibot.Web.Util
-
   def init([]), do: false
 
   def call(conn, _opts) do
     conn
     |> Plug.Conn.fetch_query_params()
-    |> Util.with_json_headers()
+    |> with_json_headers()
+  end
+
+  def with_json_headers(conn = %Plug.Conn{}) do
+    with_header(conn, "content-type", "application/json; charset=utf-8")
+  end
+
+  def with_header(conn = %Plug.Conn{}, key, value) do
+    Plug.Conn.update_resp_header(
+      conn,
+      key,
+      value,
+      &(&1 <> "; " <> value)
+    )
   end
 end
