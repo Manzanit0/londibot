4fb670bbf9b2e88b288ca09a0087b4555ce3d7c3
Manzanit0 <unico.javier@gmail.com>
2019-07-27 18:40:28 +0100

    Move behaviours to directories where implemented

--- lib/londibot/core/tfl/tfl.ex
+++ lib/londibot/core/tfl/tfl.ex
@@ -1,10 +1,3 @@
-defmodule Londibot.TFLBehaviour do
-  @callback lines() :: [String.t()]
-  @callback status(list) :: String.t()
-  @callback status(String.t()) :: String.t()
-  @callback disruptions(list) :: list
-end
-
 defmodule Londibot.TFL do
   alias Londibot.StatusChange
 
