7aadde34bca0c3b6dbc1fd29415c8a7cf700f8a0
Manzanit0 <unico.javier@gmail.com>
2019-10-09 09:50:43 +0100

    Add bar chart to public dashboard website

--- lib/londibot_web/views/dashboard_view.ex
+++ lib/londibot_web/views/dashboard_view.ex
@@ -6,11 +6,26 @@ defmodule LondibotWeb.DashboardView do
   def disruptions_data(_conn) do
     query =
       from c in "status_changes",
-      where: c.new_status != "Good Service" and c.new_status != "Service Closed",
-      select: {c.tfl_line, count(c.id)},
-      group_by: c.tfl_line,
-      order_by: [desc: count(c.id)]
+        where: c.new_status != "Good Service" and c.new_status != "Service Closed",
+        select: {
+          row_number() |> over(order_by: [desc: count(c.id)]),
+          c.tfl_line,
+          count(c.id)
+        },
+        group_by: c.tfl_line,
+        order_by: [desc: count(c.id)]
 
-    Londibot.Repo.all(query)
+    query
+    |> Londibot.Repo.all()
+    |> append_max_count()
+  end
+
+  defp append_max_count(records) do
+    max =
+      records
+      |> Enum.map(fn {_index, _line, count} -> count end)
+      |> Enum.max()
+
+    Enum.map(records, fn {row, line, count} -> {row, line, count, max} end)
   end
 end
