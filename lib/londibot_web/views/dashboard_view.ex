dd64e3072e449baf26abd51f55f5406b240cf3f6
Manzanit0 <unico.javier@gmail.com>
2019-10-18 17:04:29 +0100

    Refactor dashboard chart to make it size-relative
    Parametrized all SVG values to be calculated based on the chart
    height, and values instead of hardcoded values.
    
    Also seized the occasion to explain all the magical happening
    with some comments.

--- lib/londibot_web/views/dashboard_view.ex
+++ lib/londibot_web/views/dashboard_view.ex
@@ -18,6 +18,7 @@ defmodule LondibotWeb.DashboardView do
     query
     |> Londibot.Repo.all()
     |> append_max_count()
+    |> custom_crop_names()
   end
 
   defp append_max_count(records) do
@@ -28,4 +29,50 @@ defmodule LondibotWeb.DashboardView do
 
     Enum.map(records, fn {row, line, count} -> {row, line, count, max} end)
   end
+
+  defp custom_crop_names(records), do: Enum.map(records, &crop_name/1)
+
+  defp crop_name({_, "London Overground", _, _} = r), do: put_elem(r, 1, "Overground")
+  defp crop_name({_, "Hammersmith & City", _, _} = r), do: put_elem(r, 1, "Ham. & City")
+  defp crop_name({_, "Waterloo & City", _, _} = r), do: put_elem(r, 1, "Wat. & City")
+  defp crop_name(record), do: record
+
+  # Height of the chart in pixels
+  def max_height, do: 300
+  def bar_width, do: 35
+
+  # Rule of three formula. We don't want the height to be
+  # higher than the chart's height.
+  # Also, make all bars 20% lower than the actual total
+  # height of the chart.
+  def bar_height(max_count, current_count, chart_height),
+    do: (chart_height * current_count * 0.8) / max_count
+
+  # Since SVG bars (x, y) coordinates start at the top-left corner,
+  # To make them seem like they start at the bottom, lower them the difference.
+  def bar_y(max_count, current_count, chart_height),
+    do: chart_height - bar_height(max_count, current_count, chart_height)
+
+  def text_y(max_count, current_count, chart_height),
+    do: chart_height - max_count * 2 - current_count / 2
+
+  # Since we want bars and text aligned, we give them
+  # the same 'x' value. 40 is kind of magical ¯\_(ツ)_/¯.
+  def bar_x(position),
+    do: position * 40
+
+  def text_x(position),
+    do: position * 40
+
+  # Same applies for the transform. Magical playing ¯\_(ツ)_/¯.
+  def text_transform(current_count, position) do
+    translate_value = 70 - current_count*1.5
+    rotate_x = text_x(position)
+    rotate_y = 70 - current_count/2
+    "translate(180, #{translate_value}) rotate(-45, #{rotate_x}, #{rotate_y})"
+  end
+
+  # Some resources to understand the above:
+  # - https://css-tricks.com/transforms-on-svg-elements/
+  # - https://css-tricks.com/how-to-make-charts-with-svg/
 end
